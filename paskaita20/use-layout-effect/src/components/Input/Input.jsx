246aada0d7bd96c3eac8cca6b524e397d369376c
Modestasd848 <Modestasd848@gmail.com>
2023-04-17 18:45:12 +0300

    lesson20 class material useLayoutEffect

---
+++ paskaita20/use-layout-effect/src/components/Input/Input.jsx
@@ -0,0 +1,57 @@
+import React, { useLayoutEffect, useRef, useEffect, useState } from 'react';
+
+export default function Input({ text, setText }) {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(false);
+
+  const divRef = useRef(null);
+
+  //   function onResizeHandler() {
+  //     console.log(window.innerWidth);
+  //   }
+
+  //   useEffect(() => {
+  //     window.addEventListener('resize', onResizeHandler);
+
+  //     return () => {
+  //       window.removeEventListener('resize', onResizeHandler);
+  //     };
+  //   }, []);
+
+  //   useEffect(() => {
+  //     console.log('second useEffect');
+  //   }, [count]);
+
+  //   useEffect(() => {
+  //     console.log('useEffect prop changed');
+  //   }, [text]);
+
+  useLayoutEffect(() => {
+    if (divRef.current == null) {
+      return;
+    }
+
+    const top = divRef.current.style.top;
+    divRef.current.style.top = `${top + 200}px`;
+    console.log(top);
+  }, [show]);
+
+  //   useLayoutEffect(() => {
+  //     console.log('first useLayoutEffect');
+  //   }, [count]);
+
+  return (
+    <div>
+      {/* <button onClick={() => setCount((prev) => ++prev)}>Click Me</button>
+      <p>{count}</p>
+      <label htmlFor="">{text}</label>
+      <input type="text" onChange={(e) => setText(e.target.value)} /> */}
+      <button onClick={() => setShow((prev) => !prev)}>Click Me</button>
+      {show && (
+        <div ref={divRef} style={{ position: 'absolute' }}>
+          Labas
+        </div>
+      )}
+    </div>
+  );
+}
